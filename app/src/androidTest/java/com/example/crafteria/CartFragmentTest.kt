02d1e54e5b91cff9e3c9ac2c05b711d2128aced6
ham023 <hannanbutt023095@gmail.com>
2023-05-13 05:06:23 -0700

    comments added

--- app/src/androidTest/java/com/example/crafteria/CartFragmentTest.kt
+++ app/src/androidTest/java/com/example/crafteria/CartFragmentTest.kt
@@ -1,17 +1,11 @@
 package com.example.crafteria
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.ext.junit.rules.activityScenarioRule
-import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.contrib.NavigationViewActions
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.crafteria.homefragment.CartFragment
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
